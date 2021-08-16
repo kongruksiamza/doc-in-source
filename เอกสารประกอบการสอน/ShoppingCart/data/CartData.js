92bc7d7d703608e26984e9bbce5c6541e442c746
KongRuksiam Official <kongruksiamstudio@gmail.com>
2021-08-16 15:01:30 +0700

    add data shopping cart

---
+++ เอกสารประกอบการสอน/ShoppingCart/data/CartData.js
@@ -0,0 +1,25 @@
+const CartData = [
+    {
+        id:1,
+        name : "กระเป๋า",
+        image_url:"https://images.unsplash.com/photo-1548036328-c9fa89d128fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80",
+        price:2000,
+        quantity:1
+    },
+    {
+        id:2,
+        name : "หมวก",
+        image_url:"https://images.unsplash.com/photo-1620327467532-6ebaca6273ed?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNhcHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        price:500,
+        quantity:1
+    },
+    {
+        id:3,
+        name : "รองเท้า",
+        image_url:"https://images.unsplash.com/photo-1617606002806-94e279c22567?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
+        price:1000,
+        quantity:1
+    }
+]
+
+export default CartData
